de70f8572f9e1b60b8e08533a63a08874c676b2d
Omar Jabour <jabour.omar@hotmail.com>
2026-09-22 12:41:00 +0200

    :package: J2 : formulaire.ejs  :card_file_box:

--- j2/upload-express/server/server.js
+++ j2/upload-express/server/server.js
@@ -5,26 +5,30 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// Configuration EJS
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Permet d'accéder aux fichiers du dossier uploads
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Configuration de Multer
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, path.join(__dirname, "uploads"));
     },
+
     filename: (req, file, cb) => {
         cb(null, Date.now() + "-" + file.originalname);
     }
 });
 
-// Autoriser uniquement les images
+
 const upload = multer({
     storage: storage,
+
     fileFilter: (req, file, cb) => {
         if (file.mimetype.startsWith("image/")) {
             cb(null, true);
@@ -34,21 +38,50 @@ const upload = multer({
     }
 });
 
-// Affichage du formulaire
+
+
 app.get("/", (req, res) => {
-    res.render("form", {
+    res.render("home");
+});
+
+
+app.get("/formulaire", (req, res) => {
+    res.render("formulaire");
+});
+
+app.post("/formulaire", (req, res) => {
+    console.log(req.body);
+
+    res.send(`
+        <h1>Formulaire reçu</h1>
+
+        <p>Nom : ${req.body.nom}</p>
+
+        <p>Métier : ${req.body.metier}</p>
+
+        <p>
+            Aime Node.js :
+            ${req.body.node ? "Oui" : "Non"}
+        </p>
+
+        <a href="/">Retour à l'accueil</a>
+    `);
+});
+
+
+app.get("/upload", (req, res) => {
+    res.render("upload", {
         image: null
     });
 });
 
-// Traitement de l'upload
 app.post("/upload", upload.single("image"), (req, res) => {
-    res.render("form", {
+    res.render("upload", {
         image: req.file
     });
 });
 
-// Démarrage du serveur
+
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
